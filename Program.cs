78cf363734a145840c55d83e538d76542398ce6a
Oscar Alshammar <oscar@alshammar.se>
2025-12-12 13:31:36 +0100

    Provar med API istället

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-Deck deck = new();
+using System.Text.Json;
 
+HttpClient client = new();
+string result = await client.GetStringAsync("https://deckofcardsapi.com/api/deck/new/shuffle/?jokers_enabled=true");
 
-while (true)
-{
-    Console.WriteLine(deck.DrawTop().ToString());
-    Console.ReadLine();
-}
+Console.WriteLine(result);
+Deck deck = JsonSerializer.Deserialize<Deck>(result)!;
+Console.WriteLine(deck.deck_id);
+Console.WriteLine(deck.remaining);
 
 public enum Suit
 {
@@ -73,49 +74,51 @@ class Card
 class Deck
 {
     //    public List<Card> cards;
+    public string deck_id { get; set; }
+    public int remaining { get; set; }
 
-    private Stack<Card> _cards;
+    // private Stack<Card> _cards;
 
-    public Deck(int decks = 1)
-    {
-        _cards = new(52 * decks);
-        Build(decks);
-        Shuffle();
-    }
+    // public Deck(int decks = 1)
+    // {
+    //     _cards = new(52 * decks);
+    //     Build(decks);
+    //     Shuffle();
+    // }
 
-    private void Build(int decks)
-    {
-        _cards.Clear();
+    // private void Build(int decks)
+    // {
+    //     _cards.Clear();
 
-        for (int d = 0; d < decks; d++)
-        {
-            foreach (Suit s in Enum.GetValues(typeof(Suit)))
-            {
-                foreach (Rank r in Enum.GetValues(typeof(Rank)))
-                {
-                    _cards.Push(new Card(s, r));
-                }
-            }
-        }
+    //     for (int d = 0; d < decks; d++)
+    //     {
+    //         foreach (Suit s in Enum.GetValues(typeof(Suit)))
+    //         {
+    //             foreach (Rank r in Enum.GetValues(typeof(Rank)))
+    //             {
+    //                 _cards.Push(new Card(s, r));
+    //             }
+    //         }
+    //     }
 
-        _cards.Push(new Joker());
-        _cards.Push(new Joker());
-    }
+    //     _cards.Push(new Joker());
+    //     _cards.Push(new Joker());
+    // }
 
-    private void Shuffle()
-    {
-        _cards = new Stack<Card>(_cards.OrderBy(card => Random.Shared.Next()));
-    }
+    // private void Shuffle()
+    // {
+    //     _cards = new Stack<Card>(_cards.OrderBy(card => Random.Shared.Next()));
+    // }
 
-    public Card DrawTop()
-    {
-        return _cards.Pop();
-    }
+    // public Card DrawTop()
+    // {
+    //     return _cards.Pop();
+    // }
 
-    public void AddCard(Card cardToAdd)
-    {
-        _cards.Push(cardToAdd);
-    }
+    // public void AddCard(Card cardToAdd)
+    // {
+    //     _cards.Push(cardToAdd);
+    // }
 
-    public int Count => _cards.Count;
+    public int Count => remaining;
 }
