f21f4001123205f93df9cab0021338e7d5c31a09
Oscar Alshammar <oscar@alshammar.se>
2025-12-12 22:17:41 +0100

    fixat lite ordning samt en game class.

--- Program.cs
+++ Program.cs
@@ -1,27 +1,21 @@
-using System.Text.Json;
 
-HttpClient client = new();
-string result = await client.GetStringAsync("https://deckofcardsapi.com/api/deck/new/shuffle/?jokers_enabled=true"); // Hämtar ett nytt deck med 2 jokerar
+Game g = new();
+await g.StartAsync();
 
-Deck deck = JsonSerializer.Deserialize<Deck>(result)!;
-// Console.WriteLine(deck.deck_id);
-// Console.WriteLine(deck.Count);
 
-// Drar 5 kort och visar dem
-DrawResponse draw = await deck.DrawCardsAsync(8, client);
-if (draw.cards == null) return;
-foreach (Card card in draw.cards)
-{
-    Console.WriteLine(card);
-}
 
-List<String[]> asciiCards = draw.cards.Select(c => c.GetAscii()).ToList();
-// Print 5 lines (the height of each ASCII card)
-for (int line = 0; line < asciiCards[0].Length; line++)
-{
-    foreach (var card in asciiCards)
-    {
-        Console.Write(card[line] + "  ");   // two spaces between cards
-    }
-    Console.WriteLine();
-}
+// HttpClient client = new();
+// string result = await client.GetStringAsync("https://deckofcardsapi.com/api/deck/new/shuffle/?jokers_enabled=true"); // Hämtar ett nytt deck med 2 jokerar
+
+// Deck deck = JsonSerializer.Deserialize<Deck>(result)!;
+// // Console.WriteLine(deck.deck_id);
+// // Console.WriteLine(deck.Count);
+
+// // Drar 5 kort och visar dem
+// DrawResponse draw = await deck.DrawCardsAsync(8, client);
+// if (draw.cards == null) return;
+// foreach (Card card in draw.cards)
+// {
+//     Console.WriteLine(card);
+// }
+
